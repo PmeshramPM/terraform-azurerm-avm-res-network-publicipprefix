41239713f6b24503b07e53ddb781f2c1ba0cb385
PankajMeshramCSA <pankaj.meshram@microsoft.com>
2024-08-13 21:30:14 +0100

    public ip code push 1

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
 # TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
-resource "azurerm_resource_group" "TODO" {
+resource "azurerm_public_ip_prefix" "this" {
   location = var.location
   name     = var.name # calling code must supply the name
   tags     = var.tags
+  resource_group_name = var.resource_group_name
+  sku      = var.sku_name
+  prefix_length = var.prefix_length
 }
 
 # required AVM resources interfaces
@@ -10,16 +13,15 @@ resource "azurerm_management_lock" "this" {
   count = var.lock != null ? 1 : 0
 
   lock_level = var.lock.kind
-  name       = coalesce(var.lock.name, "lock-${var.lock.kind}")
-  scope      = azurerm_MY_RESOURCE.this.id # TODO: Replace with your azurerm resource name
-  notes      = var.lock.kind == "CanNotDelete" ? "Cannot delete the resource or its child resources." : "Cannot delete or modify the resource or its child resources."
+  name       = coalesce(var.lock.name, "lock-${var.name}")
+  scope      = azurerm_public_ip_prefix.this.id
 }
 
 resource "azurerm_role_assignment" "this" {
   for_each = var.role_assignments
 
   principal_id                           = each.value.principal_id
-  scope                                  = azurerm_resource_group.TODO.id # TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
+  scope                                  = azurerm_public_ip_prefix.this.id # TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
   condition                              = each.value.condition
   condition_version                      = each.value.condition_version
   delegated_managed_identity_resource_id = each.value.delegated_managed_identity_resource_id
